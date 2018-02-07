44bb052110c96be6de137a78a045762907011544
CrazyMax <crazy-max@users.noreply.github.com>
2018-02-07 16:00:31 +0100

    Update to 5.8.4

--- build.sh
+++ build.sh
@@ -9,18 +9,16 @@ function docker_tag_exists() {
   echo $(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2\"] | any")
 }
 
-function docker_build() {
-  echo "### Start building Artifactory $2 $3..."
+function docker_pull() {
+  echo "### Pulling Artifactory $2 $3 from $1..."
   TAG_EXISTS=$(docker_tag_exists "${DOCKER_USERNAME}/${DOCKER_REPONAME}" "$2-$3")
   echo "Check tag exists : $TAG_EXISTS"
   if [ "$3" != "latest" ] && [ "${TAG_EXISTS}" = "true" ]; then
-    echo "Tag $2-$3 already exists and is not latest..."
+    echo "Tag $2-$3 already exists and is not latest... Skipping..."
   else
-    mkdir -p ./docker/$2-$3
-    echo "FROM $1:$3" > ./docker/$2-$3/Dockerfile
-    docker build -t $2-$3 -f ./docker/$2-$3/Dockerfile ./docker/$2-$3
-    docker tag $2-$3 ${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
-    docker tag $2-$3 quay.io/${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
+    docker pull $1:$3
+    docker tag $1:$3 ${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
+    docker tag $1:$3 quay.io/${DOCKER_USERNAME}/${DOCKER_REPONAME}:$2-$3
   fi
 }
 
@@ -30,17 +28,17 @@ rm -rf ./docker
 # Artifactory oss
 while IFS= read -r version
 do
-  docker_build "docker.bintray.io/jfrog/artifactory-oss" "oss" "$version"
+  docker_pull "docker.bintray.io/jfrog/artifactory-oss" "oss" "$version"
 done < "./images/artifactory-oss.txt"
 
 # Artifactory pro
 while IFS= read -r version
 do
-  docker_build "docker.bintray.io/jfrog/artifactory-pro" "pro" "$version"
+  docker_pull "docker.bintray.io/jfrog/artifactory-pro" "pro" "$version"
 done < "./images/artifactory-pro.txt"
 
 # Nginx Artifactory pro
 while IFS= read -r version
 do
-  docker_build "docker.bintray.io/jfrog/nginx-artifactory-pro" "nginx-pro" "$version"
+  docker_pull "docker.bintray.io/jfrog/nginx-artifactory-pro" "nginx-pro" "$version"
 done < "./images/nginx-artifactory-pro.txt"
