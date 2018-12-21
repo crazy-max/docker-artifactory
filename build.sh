b4651edf210c44f79cbb54d566728e0673c39a3a
CrazyMax <crazy-max@users.noreply.github.com>
2018-12-21 20:40:00 +0100

    New registry API url

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DOCKER_USERNAME=${DOCKER_USERNAME:-crazymax}
 DOCKER_REPONAME=${DOCKER_REPONAME:-artifactory}
-TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://registry.hub.docker.com/v2/users/login/ | jq -r .token)
 
 if [ -z ${TOKEN} -o ${TOKEN} == "null" ]; then
   echo "Cannot retrieve token. Check your docker's credentials."
@@ -11,7 +11,7 @@ fi
 
 function docker_pull() {
   echo "### Pulling Artifactory $2 $3 from $1..."
-  TAG_EXISTS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${DOCKER_USERNAME}/${DOCKER_REPONAME}/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2-$3\"] | any")
+  TAG_EXISTS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://registry.hub.docker.com/v2/repositories/${DOCKER_USERNAME}/${DOCKER_REPONAME}/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2-$3\"] | any")
   echo "Check tag exists : $TAG_EXISTS"
   if [ "$3" != "latest" ] && [ "${TAG_EXISTS}" = "true" ]; then
     echo "Tag $2-$3 already exists and is not latest... Skipping..."
